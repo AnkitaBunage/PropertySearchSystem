d0ec29a1296988459f1df45fafc16183a1c44858
unknown <cogjava3194@iiht.tech>
2022-10-29 16:07:12 +0530

    adding ui n backend projs

---
+++ PropertySearchJavaBackend/PropertySearchSystem/src/main/java/com/app/PropertySearchSystem/PropertySearchSystemApplication.java
@@ -0,0 +1,13 @@
+package com.app.PropertySearchSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertySearchSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertySearchSystemApplication.class, args);
+	}
+
+}
